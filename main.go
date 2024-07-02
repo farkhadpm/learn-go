9ef434d5fc4b2ba85033aff28850fce12fb8593a
Farkhad <farkhadpm@yandex.ru>
2024-07-02 16:33:52 +0300

    call new functions

--- main.go
+++ main.go
@@ -5,6 +5,20 @@ import (
 )
 
 func main() {
-	fmt.Print("Assalamu Aleykum, brother!\n")
+	fmt.Print("Assalamu Aleykum, brother!")
+
+	fmt.Println("\n\nVariables:")
 	print_var()
+
+	fmt.Println("\n\nSumm:")
+	add_result := add(1, 2)
+	fmt.Println("x + y = ", add_result)
+
+	fmt.Println("\n\nDivision: ")
+	devide_res, err := devide(32, 0)
+	if err != nil {
+		fmt.Println("Erorr:", err)
+	}
+	fmt.Println("x / y = ", devide_res)
+
 }
